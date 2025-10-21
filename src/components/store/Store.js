08a062033895722260dc8baea47f7899238fda70
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-21 11:22:21 +0530

    Delete src/components/store/Store.js

--- src/components/store/Store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import booksReducer from '../feature/BookSlice'
-const Store = configureStore({
-  reducer:{
-    books:booksReducer
-  }
-})
-
-export default Store;
